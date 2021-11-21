6e7dc12750e6c0c3d64f09abba24b627919cdbd7
Lucas-Kobayashi <kobacarmo2@hotmail.com>
2021-11-21 19:56:54 -0300

    Primeiro exercicio de javascript

---
+++ Sintaxe e operadores (javascript)/If (ternario).js
@@ -0,0 +1,10 @@
+function comparaNumeros(num1, num2) {
+  const saoIguais = num1 === num2
+  const soma = num1 + num2
+
+  //Utilização de "if" ternario ao invés de if else comum
+  //A primeira parter antes da interrogação deve ser verdadeira
+  //Apos a interrogação vem o que deve ocorrer se for true (if) e após os dois pontos o false (else)
+  // (Condição) ? (Expr1) : (Expr2);
+  return saoIguais ? 'São iguais' : 'Não são iguais'
+}
