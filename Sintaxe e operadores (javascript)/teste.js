29b83ba9b691639c9e394560340d343a1419f063
Lucas-Kobayashi <kobacarmo2@hotmail.com>
2021-11-21 22:14:43 -0300

    assets

--- Sintaxe e operadores (javascript)/teste.js
+++ Sintaxe e operadores (javascript)/teste.js
@@ -1,35 +1,35 @@
 function comparaNumeros(num1, num2) {
-  const primeiraFrase = criaPrimeiraFrase(num1, num2)
-  const segundaFrase = criaSegundaFrase(num1, num2)
+  const primeiraFrase = criaPrimeiraFrase(num1, num2);
+  const segundaFrase = criaSegundaFrase(num1, num2);
 
-  return `${primeiraFrase} ${segundaFrase}`
+  return `${primeiraFrase} ${segundaFrase}`;
 }
 
 function criaPrimeiraFrase(num1, num2) {
-  let primeiraFrase = `Os números ${num1} e ${num2}`
-  let simNao = 'não'
+  let primeiraFrase = `Os números ${num1} e ${num2}`;
+  let simNao = 'não';
 
   if (num1 === num2) {
-    simNao = ''
+    simNao = '';
   }
 
-  return `${primeiraFrase} ${simNao} são iguais.`
+  return `${primeiraFrase} ${simNao} são iguais.`;
 }
 
 function criaSegundaFrase(num1, num2) {
-  const soma = num1 + num2
-  let comparaDez = 'menor'
-  let comparaVinte = 'menor'
+  const soma = num1 + num2;
+  let comparaDez = 'menor';
+  let comparaVinte = 'menor';
 
   if (soma > 10) {
-    comparaDez = 'maior'
+    comparaDez = 'maior';
   }
 
   if (soma > 20) {
-    comparaVinte = 'maior'
+    comparaVinte = 'maior';
   }
 
-  return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`
+  return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`;
 }
 
-console.log(comparaNumeros(15, 10))
+console.log(comparaNumeros(15, 10));
