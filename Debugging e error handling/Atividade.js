9449e58f12d0b794a538844f7e96881580178869
Lucas-Kobayashi <kobacarmo2@hotmail.com>
2022-01-19 00:07:39 -0300

    Atividade finalizada

--- Debugging e error handling/Atividade.js
+++ Debugging e error handling/Atividade.js
@@ -2,11 +2,11 @@ function validaArray(arr, num) {
   try {
     if (!arr && !num) throw new ReferenceError("Envie outro parâmetro");
 
-    if (typeof arr === "object")
+    if (typeof arr !== "object")
       throw new TypeError("Array precisa ser um objeto");
 
-    if (typeof arr === "number")
-      throw new TypeError("Array precisa ser um número");
+    if (typeof num !== "number")
+      throw new TypeError("Num precisa ser um número");
 
     if (arr.length !== num) throw new RangeError("Tamanho inválido");
 
@@ -15,15 +15,16 @@ function validaArray(arr, num) {
     if (e instanceof ReferenceError) {
       console.log("Este erro é um reference error");
       console.log(e.message);
-      console.log(e.stack);
     } else if (e instanceof TypeError) {
       console.log("Este erro é um type error");
       console.log(e.message);
-      console.log(e.stack);
     } else if (e instanceof RangeError) {
       console.log("Este erro é um range error");
       console.log(e.message);
-      console.log(e.stack);
+    } else {
+      console.log("Este erro não era esperado: " + e);
     }
   }
 }
+
+console.log(validaArray([1, 3, 5], 3));
