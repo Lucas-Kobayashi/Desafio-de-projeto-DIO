b80374f43da882623e7d0e84e4a8e370ed68c895
Lucas-Kobayashi <kobacarmo2@hotmail.com>
2022-01-30 20:52:14 -0300

    completo

---
+++ Intro-react/Aula-2/webpack-config/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <p>Olá marilene</p>
+      <p>Vinho, Tainha e muito sex.</p>
+    </div>
+  );
+};
+export default App;
