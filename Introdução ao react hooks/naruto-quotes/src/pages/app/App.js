6cd570d6f903ceb80d53db645c5d01277eccf902
Lucas-Kobayashi <kobacarmo2@hotmail.com>
2022-02-08 00:23:30 -0300

    fim

--- Introdução ao react hooks/naruto-quotes/src/pages/app/App.js
+++ Introdução ao react hooks/naruto-quotes/src/pages/app/App.js
@@ -1,11 +1,36 @@
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import narutoImg from "../../images/naruto.png";
 import { Quotes } from "../../components";
+import { getQuote } from "../../services";
+import jutsoSound from "../../sounds/jutso.mp3";
+
+const audio = new Audio(jutsoSound);
 
 export function App() {
+  const isMounted = useRef(true);
+
+  const [quoteState, setQuoteState] = useState({
+    quote: "ok",
+    speaker: "speaker"
+  });
+
+  const onUpdate = async () => {
+    const quote = await getQuote();
+
+    if (isMounted.current) {
+      audio.play();
+      setQuoteState(quote);
+    }
+  };
+
+  useEffect(() => {
+    isMounted.current = false;
+  }, []);
+
   return (
     <Content>
-      <Quotes quote={"ok"} speaker={"speaker"} />
+      <Quotes {...quoteState} onUpdate={onUpdate} />
       <NarutoImg src={narutoImg} alt="Naruto with kunai" />
     </Content>
   );
--- Introdução ao react hooks/naruto-quotes/src/pages/app/App.test.js
+++ Introdução ao react hooks/naruto-quotes/src/pages/app/App.test.js
@@ -1,14 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { App } from "./App";
+
+const response = { speaker: "Speaker", quote: "Test quote" };
+
+const server = setupServer(
+  rest.get("http://127.0.0.1:5000", (req, res, ctx) => {
+    return res(ctx.json(response));
+  })
+);
 
 test("render the app with a image, a quote and a button", () => {
   render(<App />);
 
   const buttonEl = screen.getByRole("button");
   const imageEl = screen.getByRole("img");
-  const textEl = screen.getByText(/ok/);
+  const textEl = screen.getByText(/Loading speaker.../);
 
   expect(buttonEl).toBeInTheDocument();
   expect(imageEl).toBeInTheDocument();
   expect(textEl).toBeInTheDocument();
+});
+
+test("calls api on button click and update its text", async () => {
+  render(<App />);
+
+  const buttonEl = screen.getByRole("button");
+  fireEvent.click(buttonEl);
 });
