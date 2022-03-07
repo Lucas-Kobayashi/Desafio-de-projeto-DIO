974130d1157e4a581c6f0449e0ed7045a98f1d1d
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-03-07 13:35:05 -0300

    Inicio Banco

--- Rest com node/src/routes/users.route.ts
+++ Rest com node/src/routes/users.route.ts
@@ -9,10 +9,34 @@ usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
 
 usersRoute.get(
   "/users/:uuid",
-  (req: Request, res: Response, next: NextFunction) => {
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     res.status(200).send({ uuid });
   }
 );
 
+usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  console.log(req.body);
+  res.status(201).send(newUser);
+});
+
+usersRoute.put(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+    res.status(200).send(modifiedUser);
+  }
+);
+
+usersRoute.delete(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.sendStatus(200);
+  }
+);
 export default usersRoute;
