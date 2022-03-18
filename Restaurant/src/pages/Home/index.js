d6ac1fba38ed6f07fe45691e33fbb48018c8d0df
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-03-18 12:05:18 -0300

    Carrossel finalizado

--- Restaurant/src/pages/Home/index.js
+++ Restaurant/src/pages/Home/index.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
+import Slider from 'react-slick';
+import { Card } from '../../components';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
 
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import { Container, Carousel, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptativeHeigh: true,
+  };
+
   return (
     <Wrapper>
       <Container>
@@ -19,6 +32,15 @@ const Home = () => {
             trailingIcon={<MaterialIcon role="button" icon="search" />}>
             <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
           </TextField>
+          <CarouselTitle>Em sua área</CarouselTitle>
+          <Carousel {...settings}>
+            <Card photo={restaurante} title="texto Aqui" />
+            <Card photo={restaurante} title="texto Aqui" />
+            <Card photo={restaurante} title="texto Aqui" />
+            <Card photo={restaurante} title="texto Aqui" />
+            <Card photo={restaurante} title="texto Aqui" />
+            <Card photo={restaurante} title="texto Aqui" />
+          </Carousel>
         </Search>
       </Container>
       <Map />
